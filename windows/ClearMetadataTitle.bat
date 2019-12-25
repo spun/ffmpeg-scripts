0e3587342a69c688aae62f4149486f26dfce16e0
vnm3 <vnm3@alu.ua.es>
2019-12-25 13:26:32 +0100

    New script behavior - "ClearMetadataTitle" now replaces the original file (+ backup) instead of creating a new one.

--- windows/ClearMetadataTitle.bat
+++ windows/ClearMetadataTitle.bat
@@ -1,4 +1,15 @@
 @ECHO OFF
-ECHO "%~1%"
-%~dp0/ffmpeg -loglevel warning -i "%~1" -metadata title="" -codec copy "%~p1%~n1.clear%~x1"
-timeout 5
+ECHO "%~1"
+:: Final name of the backup file and its path
+SET BACKUP_FILE_NAME="%~n1.backup%~x1"
+SET BACKUP_FILE_PATH="%~p1%~n1.backup%~x1"
+:: Run rename
+REN "%~1" %BACKUP_FILE_NAME%
+:: Check if the renaming was successful
+IF %errorlevel% gtr 0 (
+  ECHO RENAMING FAILED
+) ELSE (
+  :: Use ffmpeg to clear the title
+  %~dp0/ffmpeg -loglevel warning -i %BACKUP_FILE_PATH% -metadata title="" -codec copy "%~1"
+)
+TIMEOUT 5
