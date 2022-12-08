2fbaf7c7b30a4f3b64f93f222593f1e0e1c2eb27
spundev <spundev@gmail.com>
2022-12-08 21:35:58 +0100

    Modify M3UDown script to receive a startTime value

--- windows/M3UDown.bat
+++ windows/M3UDown.bat
@@ -1,4 +1,19 @@
-@ECHO OFF
-ECHO "%~1"
-%~dp0/ffmpeg -loglevel warning -protocol_whitelist "file,http,tcp,https,tls" -i "%~1" -bsf:a aac_adtstoasc -c copy "%~p1%~n1.mp4"
+@echo off
+echo "%~1"
+:: default start time value
+set startTime=0
+:get_start_time
+    :: start time input prompt
+    set /p startTime=Start time [in seconds] (default - %startTime%): 
+    :: check if received value is a valid number. Source: https://stackoverflow.com/a/17584764
+    set "var="&for /f "delims=0123456789" %%i in ("%startTime%") do set var=%%i
+    if defined var (
+        echo "%startTime%" is not a valid number
+        echo:
+        :: restore default and ask again
+        set startTime=0
+        goto :get_start_time
+    )
+echo Running command...
+%~dp0/ffmpeg -loglevel warning -protocol_whitelist "file,http,tcp,https,tls" -i "%~1" -ss %startTime% -bsf:a aac_adtstoasc -c copy "%~p1%~n1.mp4"
 timeout 5
