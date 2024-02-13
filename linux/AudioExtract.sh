da0405ea3412f22d36c14cf3bac7191704e3e3e6
spundev <spundev@gmail.com>
2024-02-13 11:04:43 +0100

    Fix issue when a path with spaces is used

--- linux/AudioExtract.sh
+++ linux/AudioExtract.sh
@@ -4,4 +4,4 @@ FILEPATH=$1
 DIR="$(dirname "${FILEPATH}")"
 FILE="$(basename "${FILEPATH}")"
 FILENAME=${FILE%.*}
-ffmpeg -i $FILEPATH -q:a 0 -map a "${DIR}/${FILENAME}.mp3"
+ffmpeg -i "$FILEPATH" -q:a 0 -map a "${DIR}/${FILENAME}.mp3"
